ed4d792a8e36c8789ca9c042017bbb9151ba87c7
Alejandro Sierra <alejandrosierraballesta@gmail.com>
2021-01-12 18:33:09 +0100

    arreglo02 ejercicio06 BD

--- bbdd/bda06/06p05.sql
+++ bbdd/bda06/06p05.sql
@@ -1,8 +1,8 @@
-source 06p04.sql;
+-- source 06p04.sql;
 use biblioteca;
 
--- truncate LIBRO;
--- truncate PRESTAMO;
+truncate table PRESTAMO;
+truncate table LIBRO;
 
 insert into LIBRO values (null,"Historia de España","J. Pérez",5,"HIS");
 insert into LIBRO values (null,"Reina Roja","J. Gómez-Jurado",33,"NOV");
