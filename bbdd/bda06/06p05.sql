1a3d3aeb1914caba6d6a7a5a95a0be31362b7048
Alejandro Sierra <alejandrosierraballesta@gmail.com>
2021-01-12 18:53:14 +0100

    intentando load infile

--- bbdd/bda06/06p05.sql
+++ bbdd/bda06/06p05.sql
@@ -1,11 +1,16 @@
--- source 06p04.sql;
+source 06p04.sql;
 use biblioteca;
 
-truncate table PRESTAMO;
-truncate table LIBRO;
+-- truncate table PRESTAMO;
+-- truncate table LIBRO;
 
 insert into LIBRO values (null,"Historia de España","J. Pérez",5,"HIS");
 insert into LIBRO values (null,"Reina Roja","J. Gómez-Jurado",33,"NOV");
 
 insert into PRESTAMO set codLibro=1, codSocio=1,fentrega="20/10/15";
 insert into PRESTAMO set codLibro=2, codSocio=1,fentrega="20/12/25";
+
+
+-- load data local infile '/home/alejandro/Escritorio/vscode/bbdd/bda06/06p03datos.txt' into table FABRICANTE fields terminiated by '\t';
+
+-- load data local infile '/home/alejandro/Escritorio/vscode/bbdd/bda06/06p03datos.txt' into table FABRICANTE terminiated by '\t';
