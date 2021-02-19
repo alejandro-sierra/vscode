ffef7f954462607313af00f841de864c4b5dcca9
Alejandro Sierra <alejandrosierraballesta@gmail.com>
2021-02-20 00:53:46 +0100

    ejercicios tema 08 empezados

---
+++ bbdd/bda08/08p01.sql
@@ -0,0 +1,75 @@
+-- BBDD DE TIENDA
+
+-- 1. Devuelve una lista con el nombre del producto, precio y nombre de fabricante de todos los
+-- productos de la base de datos.
+select p.nombre as nombreProducto ,precio,f.nombre as nombreFabricante from producto p join fabricante f on p.codigo_fabricante=f.c odigo;
+
+-- 2. Devuelve una lista con el nombre del producto, precio y nombre de fabricante de todos los
+-- productos de la base de datos. Ordene el resultado por el nombre del fabricante, por orden
+-- alfabético.
+select p.nombre as nombreProducto ,precio,f.nombre as nombreFabricante from producto p join fabricante f on p.codigo_fabricante=f.codigo order by f.nombre;
+
+-- 3. Devuelve una lista con el código del producto, nombre del producto, código del fabricante y
+-- nombre del fabricante, de todos los productos de la base de datos.
+select p.codigo as codigoProducto,p.nombre as nombreProducto,f.codigo as codigoFabricante,f.nombre as nombreFabricante from producto p join fabricante f on p.codigo_fabricante=f.codigo;
+
+-- 4. Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más
+-- barato.
+select p.nombre as nombreProducto,precio,f.nombre as nombreFabricante
+
+-- 5. Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más
+-- caro.
+uu4391940
+-- 6. Devuelve una lista de todos los productos del fabricante Lenovo.
+
+-- 7. Devuelve una lista de todos los productos del fabricante Crucial que tengan un precio mayor
+-- que 200€.
+
+-- 8. Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packardy
+-- Seagate. Sin utilizar el operador IN.
+
+-- 9. Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packardy
+-- Seagate. Utilizando el operador IN.
+
+-- 10. Devuelve un listado con el nombre y el precio de todos los productos de los fabricantes cuyo
+-- nombre termine por la vocal e.
+
+-- 11. Devuelve un listado con el nombre y el precio de todos los productos cuyo nombre de
+-- fabricante contenga el carácter w en su nombre.
+
+-- 12. Devuelve un listado con el nombre de producto, precio y nombre de fabricante, de todos los
+-- productos que tengan un precio mayor o igual a 180€. Ordene el resultado en primer lugar por
+-- el precio (en orden descendente) y en segundo lugar por el nombre (en orden ascendente)
+
+-- 13.Devuelve un listado con el código y el nombre de fabricante, solamente de aquellos fabricantesque tienen productos asociados en la base de datos.
+
+-- 17. Calcula el número total de productos que hay en la tabla productos.
+
+-- 18. Calcula el número total de fabricantes que hay en la tabla fabricante.
+
+-- 19. Calcula el número de valores distintos de código de fabricante aparecen en la tabla productos.
+
+-- 20. Calcula la media del precio de todos los productos.
+
+-- 21. Calcula el precio más barato de todos los productos.
+
+-- 22. Calcula el precio más caro de todos los productos.
+
+-- 23. Lista el nombre y el precio del producto más barato.
+
+-- 24. Lista el nombre y el precio del producto más caro.
+
+-- 25. Calcula la suma de los precios de todos los productos.
+
+-- 26. Calcula el número de productos que tiene el fabricante Asus.
+
+-- 27. Calcula la media del precio de todos los productos del fabricante Asus.
+
+-- 28. Calcula el precio más barato de todos los productos del fabricante Asus.
+
+-- 29. Calcula el precio más caro de todos los productos del fabricante Asus.
+
+-- 30. Calcula la suma de todos los productos del fabricante Asus.
+
+-- 31.Muestra el precio máximo, precio mínimo, precio medio y el número total de productos que
+-- tiene el fabricante Crucial.
--- bbdd/bda08/08p02.sql
+++ bbdd/bda08/08p02.sql
@@ -1,73 +1,68 @@
--- 1. Devuelve una lista con el nombre del producto, precio y nombre de fabricante de todos los
--- productos de la base de datos.
-select p.nombre as nombreProducto ,precio,f.nombre as nombreFabricante from producto p join fabricante f on p.codigo_fabricante=f.c odigo;
+-- BBDD DE VENTAS
 
--- 2. Devuelve una lista con el nombre del producto, precio y nombre de fabricante de todos los
--- productos de la base de datos. Ordene el resultado por el nombre del fabricante, por orden
--- alfabético.
-select p.nombre as nombreProducto ,precio,f.nombre as nombreFabricante from producto p join fabricante f on p.codigo_fabricante=f.codigo order by f.nombre;
+-- 1. Devuelve un listado con todos los pedidos que se han realizado. Los pedidos deben estar
+-- ordenados por la fecha de realización, mostrando en primer lugar los pedidos más recientes.
 
--- 3. Devuelve una lista con el código del producto, nombre del producto, código del fabricante y
--- nombre del fabricante, de todos los productos de la base de datos.
-select p.codigo as codigoProducto,p.nombre as nombreProducto,f.codigo as codigoFabricante,f.nombre as nombreFabricante from producto p join fabricante f on p.codigo_fabricante=f.codigo;
+-- 2. Devuelve todos los datos de los dos pedidos de mayor valor.
 
--- 4. Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más
--- barato.
-select p.nombre as nombreProducto,precio,f.nombre as nombreFabricante
+-- 3. Devuelve un listado con los identificadores de los clientes que han realizado algún pedido.
+-- Tenga en cuenta que que no debe mostrar identificadores que estén repetidos.
 
--- 5. Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más
--- caro.
+-- 4. Devuelve un listado de todos los pedidos que se realizaron durante el año 2017, cuya cantidad
+-- total sea superior a 500€.
 
--- 6. Devuelve una lista de todos los productos del fabricante Lenovo.
+-- 5. Devuelve un listado con el nombre y los apellidos de los comerciales que tienen una comisión
+-- entre 0.05 y 0.11.
 
--- 7. Devuelve una lista de todos los productos del fabricante Crucial que tengan un precio mayor
--- que 200€.
+-- 6. Devuelve el valor de la comisión de mayor valor que existe en la tabla comercial.
 
--- 8. Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packardy
--- Seagate. Sin utilizar el operador IN.
+-- 7. Devuelve el identificador, nombre y primer apellido de aquellos clientes cuyo segundo apellido
+-- no es NULL. El listado deberá estar ordenado alfabéticamente por apellidos y nombre.
 
--- 9. Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packardy
--- Seagate. Utilizando el operador IN.
+-- 8. Devuelve un listado de los nombres de los clientes que empiezan por A y terminan por n y
+-- también los nombres que empiezan por P. El listado deberá estar ordenado alfabéticamente.
 
--- 10. Devuelve un listado con el nombre y el precio de todos los productos de los fabricantes cuyo
--- nombre termine por la vocal e.
+-- 9. Devuelve un listado de los nombres de los clientes que no empiezan por A. El listado deberá
+-- estar ordenado alfabéticamente.
 
--- 11. Devuelve un listado con el nombre y el precio de todos los productos cuyo nombre de
--- fabricante contenga el carácter w en su nombre.
+-- 10. Devuelve un listado con los nombres de los comerciales que terminan por el o o. Tenga en
+-- cuenta que se deberán eliminar los nombres repetidos.
 
--- 12. Devuelve un listado con el nombre de producto, precio y nombre de fabricante, de todos los
--- productos que tengan un precio mayor o igual a 180€. Ordene el resultado en primer lugar por
--- el precio (en orden descendente) y en segundo lugar por el nombre (en orden ascendente)
+-- 11. Devuelve un listado con el identificador, nombre y los apellidos de todos los clientes que han
+-- realizado algún pedido. El listado debe estar ordenado alfabéticamente y se deben eliminar los
+-- elementos repetidos.
 
--- 13.Devuelve un listado con el código y el nombre de fabricante, solamente de aquellos fabricantesque tienen productos asociados en la base de datos.
+-- 12. Devuelve un listado que muestre todos los pedidos que ha realizado cada cliente. El resultado
+-- debe mostrar todos los datos de los pedidos y del cliente. El listado debe mostrar los datos de
+-- los clientes ordenados alfabéticamente.
 
--- 17. Calcula el número total de productos que hay en la tabla productos.
+-- 13. Devuelve un listado que muestre todos los pedidos en los que ha participado un comercial. El
+-- resultado debe mostrar todos los datos de los pedidos y de los comerciales. El listado debe
+-- mostrar los datos de los comerciales ordenados alfabéticamente.
 
--- 18. Calcula el número total de fabricantes que hay en la tabla fabricante.
+-- 14. Devuelve un listado que muestre todos los clientes, con todos los pedidos que han realizado y
+-- con los datos de los comerciales asociados a cada pedido.
 
--- 19. Calcula el número de valores distintos de código de fabricante aparecen en la tabla productos.
+-- 15. Devuelve un listado de todos los clientes que realizaron un pedido durante el año 2017, cuya
+-- cantidad esté entre 300 € y 1000 €.
 
--- 20. Calcula la media del precio de todos los productos.
+-- 16. Devuelve el nombre y los apellidos de todos los comerciales que ha participado en algún
+-- pedido realizado por María Santana Moreno.
 
--- 21. Calcula el precio más barato de todos los productos.
+-- 17. Devuelve el nombre de todos los clientes que han realizado algún pedido con el comercial
+-- Daniel Sáez Vega.
 
--- 22. Calcula el precio más caro de todos los productos.
+-- 24. Calcula la cantidad total que suman todos los pedidos que aparecen en la tabla pedido.
 
--- 23. Lista el nombre y el precio del producto más barato.
+-- 25. Calcula la cantidad media de todos los pedidos que aparecen en la tabla pedido.
 
--- 24. Lista el nombre y el precio del producto más caro.
+-- 26. Calcula el número total de comerciales distintos que aparecen en la tabla pedido.
 
--- 25. Calcula la suma de los precios de todos los productos.
+-- 27. Calcula el número total de clientes que aparecen en la tabla cliente.
 
--- 26. Calcula el número de productos que tiene el fabricante Asus.
+-- 28. Calcula cuál es la mayor cantidad que aparece en la tabla pedido.
 
--- 27. Calcula la media del precio de todos los productos del fabricante Asus.
+-- 29. Calcula cuál es la menor cantidad que aparece en la tabla pedido.
 
--- 28. Calcula el precio más barato de todos los productos del fabricante Asus.
-
--- 29. Calcula el precio más caro de todos los productos del fabricante Asus.
-
--- 30. Calcula la suma de todos los productos del fabricante Asus.
-
--- 31.Muestra el precio máximo, precio mínimo, precio medio y el número total de productos que
--- tiene el fabricante Crucial.
+-- 30. Calcula cuál es el valor máximo de categoría para cada una de las ciudades que aparece en la
+-- tabla cliente.
