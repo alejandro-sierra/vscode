faf3503118dcacba22d55c02a34b04ef4e4d700e
Alejandro Sierra <alejandrosierraballesta@gmail.com>
2021-02-20 11:55:48 +0100

    BD08EJ01-02 a medias

--- bbdd/bda08/08p01.sql
+++ bbdd/bda08/08p01.sql
@@ -15,39 +15,51 @@ select p.codigo as codigoProducto,p.nombre as nombreProducto,f.codigo as codigoF
 
 -- 4. Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más
 -- barato.
-select p.nombre as nombreProducto,precio,f.nombre as nombreFabricante
+select p.nombre as nombreProducto,precio,f.nombre as nombreFabricante from producto p join fabricante f on p.codigo_fabricante=f.codigo order by precio limit 1;
 
 -- 5. Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más
 -- caro.
-uu4391940
+select p.nombre as nombreProducto,precio,f.nombre as nombreFabricante from producto p join fabricante f on p.codigo_fabricante=f.codigo order by precio desc limit 1;
+
 -- 6. Devuelve una lista de todos los productos del fabricante Lenovo.
+select * from producto p join fabricante f on p.codigo_fabricante=f.codigo where f.nombre="Lenovo";
 
 -- 7. Devuelve una lista de todos los productos del fabricante Crucial que tengan un precio mayor
 -- que 200€.
+select * from producto p join fabricante f on p.codigo_fabricante=f.codigo where f.nombre="Crucial" and precio>200;
 
--- 8. Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packardy
+-- 8. Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packard y
 -- Seagate. Sin utilizar el operador IN.
+select * from producto p join fabricante f on p.codigo_fabricante=f.codigo where f.nombre="Asus" or f.nombre="Hewlett-Packard" or f.nombre="Seagate";
 
 -- 9. Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packardy
 -- Seagate. Utilizando el operador IN.
+select * from producto p join fabricante f on p.codigo_fabricante=f.codigo where f.nombre in ("Asus","Hewlett-Packard","Seagate");
 
 -- 10. Devuelve un listado con el nombre y el precio de todos los productos de los fabricantes cuyo
 -- nombre termine por la vocal e.
+select p.nombre,precio,f.nombre from producto p join fabricante f on p.codigo_fabricante=f.codigo where f.nombre like '%e';
 
 -- 11. Devuelve un listado con el nombre y el precio de todos los productos cuyo nombre de
 -- fabricante contenga el carácter w en su nombre.
+select p.nombre,precio,f.nombre from producto p join fabricante f on p.codigo_fabricante=f.codigo where f.nombre like '%w%';
 
 -- 12. Devuelve un listado con el nombre de producto, precio y nombre de fabricante, de todos los
 -- productos que tengan un precio mayor o igual a 180€. Ordene el resultado en primer lugar por
 -- el precio (en orden descendente) y en segundo lugar por el nombre (en orden ascendente)
+select p.nombre,precio,f.nombre from producto p join fabricante f on p.codigo_fabricante=f.codigo where precio>=180 order by precio desc,f.nombre asc;
 
 -- 13.Devuelve un listado con el código y el nombre de fabricante, solamente de aquellos fabricantesque tienen productos asociados en la base de datos.
+select distinct f.codigo,f.nombre from fabricante f join producto p on f.codigo=p.codigo_fabricante;
 
 -- 17. Calcula el número total de productos que hay en la tabla productos.
+select count(*) from producto; 
 
 -- 18. Calcula el número total de fabricantes que hay en la tabla fabricante.
+select count(*) from fabricante; 
 
 -- 19. Calcula el número de valores distintos de código de fabricante aparecen en la tabla productos.
+
 
 -- 20. Calcula la media del precio de todos los productos.
 
