fef2535d2a52fad0af334d686e8f1eae88975b31
Alejandro Sierra <alejandrosierraballesta@gmail.com>
2021-03-03 11:43:06 +0100

    08p03 finished

--- bbdd/bda08/08p03.sql
+++ bbdd/bda08/08p03.sql
@@ -86,7 +86,7 @@ select c.nombre_cliente, e.nombre as "nombre empleado" from cliente c join pago 
 -- 19.Muestra el nombre de los clientes que no hayan realizado pagos junto con el nombre de sus
 -- representantes de ventas.
 select c.nombre_cliente, e.nombre as "nombre empleado" from cliente c join pago p on c.codigo_cliente=p.codigo_cliente --preguntar
-                                        join empleado e on c.codigo_empleado_rep_ventas=e.codigo_empleado;
+                                        join empleado e on c.codigo_empleado_rep_ventas=e.codigo_empleado where p.forma_pago is null;
 
 -- 20. Devuelve el nombre de los clientes que han hecho pagos y el nombre de sus representantes
 -- junto con la ciudad de la oficina a la que pertenece el representante.
@@ -97,7 +97,7 @@ select c.nombre_cliente, e.nombre as "nombre empleado", o.ciudad from cliente c 
 -- 21. Devuelve el nombre de los clientes que no hayan hecho pagos y el nombre de sus
 -- representantes junto con la ciudad de la oficina a la que pertenece el representante.
 select c.nombre_cliente, e.nombre as "nombre empleado" from cliente c join pago p on c.codigo_cliente=p.codigo_cliente --preguntar
-                                        join empleado e on c.codigo_empleado_rep_ventas=e.codigo_empleado;
+                                        join empleado e on c.codigo_empleado_rep_ventas=e.codigo_empleado where p.forma_pago is null;
 
 -- 22. Lista la dirección de las oficinas que tengan clientes en Fuenlabrada.
 select o.linea_direccion1 from oficina o join empleado e on o.codigo_oficina=e.codigo_oficina
@@ -121,43 +121,68 @@ select distinct c.nombre_cliente,pro.gama from cliente c join pedido ped on ped.
 
 -- Consultas multitabla (Composición externa)
 -- 27. Devuelve un listado que muestre solamente los clientes que no han realizado ningún pago.
+select distinct c.codigo_cliente,c.nombre_cliente from cliente c left join pago p on c.codigo_cliente=p.codigo_cliente where forma_pago is null; 
 
 -- 28. Devuelve un listado que muestre solamente los clientes que no han realizado ningún pedido.
+select distinct c.codigo_cliente,c.nombre_cliente from cliente c left join pedido p on c.codigo_cliente=p.codigo_cliente where fecha_pedido is null; 
 
 -- 29. Devuelve un listado que muestre los clientes que no han realizado ningún pago y los que no
 -- han realizado ningún pedido.
+select distinct c.codigo_cliente,c.nombre_cliente from cliente c left join pedido ped on c.codigo_cliente=ped.codigo_cliente
+                                                                   left join pago pag on c.codigo_cliente=pag.codigo_cliente where fecha_pedido is null and forma_pago is null;
 
 -- 30. Devuelve un listado que muestre solamente los empleados que no tienen una oficina asociada.
-
+select e.nombre as "nombre empleado" from empleado e left join oficina o on e.codigo_oficina=o.codigo_oficina where e.codigo_oficina is null; --preguntar
+ 
 -- 31. Devuelve un listado que muestre solamente los empleados que no tienen un cliente asociado.
+select e.codigo_empleado,e.nombre as "nombre empleado" from empleado e left join cliente c on c.codigo_empleado_rep_ventas=e.codigo_empleado where codigo_empleado_rep_ventas is null;
 
 -- 32. Devuelve un listado que muestre solamente los empleados que no tienen un cliente asociado
 -- junto con los datos de la oficina donde trabajan.
+select * from empleado e left join oficina o on o.codigo_oficina=e.codigo_oficina
+                                                 left join cliente c on c.codigo_empleado_rep_ventas=e.codigo_empleado where codigo_empleado_rep_ventas is null;
 
 -- 33. Devuelve un listado que muestre los empleados que no tienen una oficina asociada y los que
 -- no tienen un cliente asociado.
+select e.codigo_empleado,e.nombre as "nombre empleado" from empleado e left join cliente c on c.codigo_empleado_rep_ventas=e.codigo_empleado --preguntar
+                                                                            join oficina o on o.codigo_oficina=e.codigo_oficina where o.codigo_oficina is null and 
+                                                                            codigo_empleado_rep_ventas is null;
 
 -- 34. Devuelve un listado de los productos que nunca han aparecido en un pedido.
+select * from producto pro left join detalle_pedido dp on dp.codigo_producto=pro.codigo_producto where codigo_pedido is null;
 
 -- 35. Devuelve un listado de los productos que nunca han aparecido en un pedido. El resultado debe
 -- mostrar el nombre, la descripción y la imagen del producto.
+select pro.nombre as "nombre producto", pro.descripcion from producto pro left join detalle_pedido dp on dp.codigo_producto=pro.codigo_producto
+                                                                     where codigo_pedido is null; --preguntar
 
 -- 36. Devuelve las oficinas donde no trabajan ninguno de los empleados que hayan sido los
 -- representantes de ventas de algún cliente que haya realizado la compra de algún producto de
 -- la gama Frutales.
+select --preguntar
+
 -- 37. Devuelve un listado con los clientes que han realizado algún pedido pero no han realizado
 -- ningún pago.
+select c.codigo_cliente,c.nombre_cliente from cliente c left join pedido p on c.codigo_cliente=p.codigo_cliente
+                        left join pago pag on pag.codigo_cliente=c.codigo_cliente where pag.codigo_cliente is null;
 
 -- 38. Devuelve un listado con los datos de los empleados que no tienen clientes asociados y el
 -- nombre de su jefe asociado.
+select e1.nombre as "nombre jefe", e2.nombre as "nombre empleado",c.codigo_cliente  from empleado e1 left join cliente c on c.codigo_empleado_rep_ventas=e.codigo_empleado
+                            left join empleado e2 on e1.codigo_empleado=e2.codigo_jefe where codigo_empleado_rep_ventas is null; --preguntar
 
 -- Consultas con funciones agregadas
 -- 39. ¿Cuántos empleados hay en la compañía?
+select count(*) from empleado;
 
 -- 40. ¿Cuál fue el pago medio en 2009?
+select avg(total) from pago where year(fecha_pago)='2009';
 
 -- 41. Calcula el precio de venta del producto más caro y más barato en una misma consulta.
+select max(precio_venta),min(precio_venta) from producto;
 
 -- 42. Calcula el número de clientes que tiene la empresa.
+select count(*) from cliente;
 
 -- 43. ¿Cuántos clientes tiene la ciudad de Madrid?
+select count(*) from cliente where ciudad="Madrid";
