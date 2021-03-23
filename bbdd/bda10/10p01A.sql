7197417c2fe16199b15adcfd6dfb545925b4aaac
Alejandro Sierra <alejandrosierraballesta@gmail.com>
2021-03-23 17:50:59 +0100

    vistas

--- bbdd/bda10/10p01A.sql
+++ bbdd/bda10/10p01A.sql
@@ -22,6 +22,8 @@ show index from jardineria.producto;
 
 --Este tardará más ya que para buscar en el nombre no tenemos un indice por lo que tendra que realizar tantas comparaciones como
 --palabra tengan todos los nombre de la tabla producto, por lo que tendrá que hacer 276 comparaiones lo que significa que tardará más.
+create index idx_nombre_producto on producto(nombre);
+--podemos crear este indice para que vaya mas rapida la consulta
 
 
 -- 3.- ¿Cuál de las dos consultas será más eficiente?.
@@ -35,6 +37,15 @@ show index from jardineria.producto;
 -- 4.- Optimiza la siguiente consulta creando índices cuando sea necesario.
 -- SELECT * FROM cliente INNER JOIN pedido ON cliente.codigo_cliente =
 -- pedido.codigo_cliente WHERE cliente.nombre_cliente LIKE 'A%';
-create index idx_codigo_cliente on pedido(codigo_cliente);
 
+create index idx_codigo_cliente on pedido(codigo_cliente); --mal
 --Creamos un indice en la tabla pedido para el codigo del cliente lo que evitara que busque en toda la tabla de pedidos.
+
+
+create fulltext index idx_nombre_cliente on cliente(nombre_cliente); 
+--tenemos que crear un fulltext porque esta buscando dentro del contenido, no en la columna la completo
+
+select * 
+from cliente inner join pedido on cliente.codigo_cliente = pedido.codigo_cliente 
+where math (cliente.nombre_cliente) against ('A%');
+--cambiamos la consulta para que utilize el indice
---
+++ bbdd/bda10/10p01B.sql
@@ -0,0 +1,9 @@
+-- B) A partir de la base de datos de jardinería:
+-- 1. Crea vistas que muestren los siguientes datos:
+    -- 1. clienteSinPedido: Clientes que no han hecho pedidos.
+    -- 2. clientePayPal: Clientes que han hecho pagos con PayPal.
+    -- 3. productosSinPedido: Productos que no se han pedido
+    -- 4. clienteMadridPedido: Clientes madrileños que han realizado pedidos
+
+-- 2. Inserta una fila en cada una de las vistas creadas en el ejercicio anterior. ¿Has podido
+-- realizarlo en todas las vistas?
