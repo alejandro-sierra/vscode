eec0845f5cc31518fb32d5e96b9528357476d9f7
alejandro-sierra <alejandrosierraballesta@gmail.com>
2021-04-27 08:18:08 +0200

    11p02 terminado

--- bda11/11p02.sql
+++ bda11/11p02.sql
@@ -21,13 +21,13 @@ CREATE TABLE 24secuencia (
 DELIMITER //
 CREATE or replace PROCEDURE 25secuenciaWhile(IN valorInicial INT UNSIGNED)
 BEGIN	
-DECLARE contador INT;
-TRUNCATE TABLE 24secuencia;
-SET contador = valorInicial;
-WHILE contador > 0 DO
-INSERT INTO 24secuencia VALUES (contador); 
-SET contador = contador - 1;
-END WHILE;
+  DECLARE contador INT;
+  TRUNCATE TABLE 24secuencia;
+  SET contador = valorInicial;
+  WHILE contador > 0 DO
+    INSERT INTO 24secuencia VALUES (contador); 
+    SET contador = contador - 1;
+  END WHILE;
 END
 //
 DELIMITER ;
@@ -39,14 +39,14 @@ SELECT * FROM 24secuencia;
 DELIMITER //
 CREATE or replace PROCEDURE 26secuenciaRepeat(IN valorInicial INT UNSIGNED)
 BEGIN	
-DECLARE contador INT;
-TRUNCATE TABLE 24secuencia;
-SET contador = valorInicial;
-REPEAT
-INSERT INTO 24secuencia VALUES (contador); 
-SET contador = contador - 1;
-UNTIL contador < 0
-END REPEAT;
+  DECLARE contador INT;
+  TRUNCATE TABLE 24secuencia;
+  SET contador = valorInicial;
+  REPEAT
+    INSERT INTO 24secuencia VALUES (contador); 
+    SET contador = contador - 1;
+  UNTIL contador < 0
+  END REPEAT;
 END
 //
 DELIMITER ;
@@ -54,19 +54,20 @@ CALL 26secuenciaRepeat(6);
 SELECT * FROM 24secuencia;
 
 -- 7) Vuelve a resolver el ejercicio pero ahora con un bucle
--- LOOP (27secuenciaLoop). --no funciona
+-- LOOP (27secuenciaLoop). --revisar
 DELIMITER //
 CREATE or replace PROCEDURE 27secuenciaLoop(IN valorInicial INT UNSIGNED)
 BEGIN	
-DECLARE contador INT;
-TRUNCATE TABLE 24secuencia;
-SET contador = valorInicial;
-bucle: LOOP
-IF contador > 0 THEN
-LEAVE INSERT INTO 24secuencia VALUES (contador); 
-END IF;
-SET contador = contador - 1;
-END LOOP;
+  DECLARE contador INT;
+  TRUNCATE TABLE 24secuencia;
+  SET contador = valorInicial;
+  bucle: LOOP
+    IF contador > 0 THEN
+      LEAVE INSERT INTO 24secuencia VALUES (contador); 
+    END IF;
+    
+    SET contador = contador - 1;
+  END LOOP;
 END
 //
 DELIMITER ;
@@ -77,6 +78,13 @@ SELECT * FROM 24secuencia;
 -- dos tablas (28par, 28impar). Ambas deben
 -- contener una única columna llamada numero de tipo
 -- INT UNSIGNED.
+CREATE TABLE 28par (
+  numero INT UNSIGNED PRIMARY KEY
+);
+
+CREATE TABLE 28impar (
+  numero INT UNSIGNED PRIMARY KEY
+);
 
 -- 9) Crea un procedimiento (29paresImpares) que
 -- reciba un parámetro de entrada llamado tope de tipo
@@ -84,10 +92,75 @@ SELECT * FROM 24secuencia;
 -- 28par aquellos números pares que existan entre el
 -- número 1 y el tope. De igual manera, insertará los impares en 28impar.
 -- -- El procedimiento deberá eliminar el contenido de la
--- -- tabla antes de insertar los nuevos valores.
+-- -- tabla antes de insertar los nuevos valores. --revisar
+DELIMITER //
+CREATE or replace PROCEDURE 29paresImpares(IN tope INT UNSIGNED)
+BEGIN	
+  DECLARE contador INT;
+  TRUNCATE TABLE 28par;
+  TRUNCATE TABLE 28impar;
+  SET contador = tope;
+  WHILE contador > 0 DO
+    IF (tope % 2) = 0 THEN
+    	INSERT INTO 28par VALUES (contador);
+    ELSE
+    	INSERT INTO 28impar VALUES (contador);
+    END IF;
+    SET contador = contador - 1;
+  END WHILE;
+END
+//
+DELIMITER ;
+CALL 29paresImpares(6);
+SELECT * FROM 28par;
+SELECT * FROM 28impar;
 
 -- 10)Resuelve el ejercicio anterior pero utilizando un
--- bucle REPEAT (210secuenciaRepeat).
+-- bucle REPEAT (210secuenciaRepeat). --revisar
+DELIMITER //
+CREATE or replace PROCEDURE 210secuenciaRepeat(IN tope INT UNSIGNED)
+BEGIN	
+  DECLARE contador INT;
+  TRUNCATE TABLE 28par;
+  TRUNCATE TABLE 28impar;
+  SET contador = tope;
+  REPEAT
+    IF (tope % 2) = 0 THEN
+    	INSERT INTO 28par VALUES (contador);
+    ELSE
+    	INSERT INTO 28impar VALUES (contador);
+    END IF;
+    SET contador = contador - 1;
+  UNTIL contador > 0
+  END REPEAT;
+END
+//
+DELIMITER ;
+CALL 210secuenciaRepeat(6);
+SELECT * FROM 28par;
+SELECT * FROM 28impar;
 
 -- 11)Vuelve a resolver el ejercicio pero ahora con un
--- bucle LOOP (211secuenciaLoop).
+-- bucle LOOP (211secuenciaLoop). -revisar
+DELIMITER //
+CREATE or replace PROCEDURE 211secuenciaLoop(IN tope INT UNSIGNED)
+BEGIN	
+  DECLARE contador INT;
+  TRUNCATE TABLE 28par;
+  TRUNCATE TABLE 28impar;
+  SET contador = tope;
+  bucle : LOOP
+  IF contador > 0 THEN
+    LEAVE IF (tope % 2) = 0 THEN
+    	INSERT INTO 28par VALUES (contador);
+    ELSE
+    	INSERT INTO 28impar VALUES (contador);
+    END IF;
+    SET contador = contador - 1;
+  END LOOP;
+END
+//
+DELIMITER ;
+CALL 211secuenciaLoop(6);
+SELECT * FROM 28par;
+SELECT * FROM 28impar;
