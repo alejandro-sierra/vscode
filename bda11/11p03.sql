2f12159c3faeabc2f794ae6e492d0e04a4f813ae
alejandro-sierra <alejandrosierraballesta@gmail.com>
2021-04-27 18:57:11 +0200

    11p03 preparado

--- bda11/11p03.sql
+++ bda11/11p03.sql
@@ -1,8 +1,10 @@
 -- 1) Escribe una función (31par) que reciba un entero como
 -- entrada y devuelva TRUE si el número es par o FALSE
 -- en caso contrario.
+
 -- 2) Escribe una función (32mayor2) que tras recibir dos
 -- enteros, devuelva el mayor de los dos.
+
 -- 3) Escribe una función (33mayor3) que tras recibir tres
 -- enteros, devuelva el mayor de los tres.
 
@@ -10,6 +12,40 @@
 -- devuelva como salida el número de años que han
 -- transcurrido entre dos fechas que se reciben como
 -- parámetros de entrada.
--- Por ejemplo, si pasamos como parámetros de entrada
--- las fechas 2021-01-01 y 2011-01-01 la función tiene que
--- devolver que han pasado 10 años.
+-- --Por ejemplo, si pasamos como parámetros de entrada
+-- --las fechas 2021-01-01 y 2011-01-01 la función tiene que
+-- --devolver que han pasado 10 años.
+-- --Debes emplear las siguientes funciones:
+-- -- --DATEDIFF: https://mariadb.com/kb/en/datediff/
+-- -- --TRUNCATE: https://mariadb.com/kb/en/truncate/
+
+-- 5) Escribe una función (35diaSemana) que a partir de una
+-- fecha, devuelva el día de la semana mediante texto
+-- (Lunes, Martes, ....).
+-- --Recuerda que en el ejercicio 17diaSemana mostrabas
+-- --el día de la semana a partir de un número.
+-- --Debes emplear la siguiente función:
+-- -- --WEEKDAY: https://mariadb.com/kb/en/weekday/
+
+-- A partir de la base de datos tienda:
+-- 1) Escribe una función (41totalProductos) que
+-- devuelva el número total de productos que hay en la
+-- tabla productos.
+
+-- 2) Escribe una función
+-- (42precioMedioPorFabricante) que devuelva el
+-- valor medio del precio de los productos de un
+-- determinado fabricante que se recibirá como
+-- parámetro de entrada.
+-- --El parámetro de entrada será el nombre del fabricante.
+
+-- 3) Escribe una función
+-- (43precioMaxPorFabricante) que devuelva el
+-- valor máximo del precio de los productos de un
+-- determinado fabricante que se recibirá como parámetro de entrada.
+
+-- 4) Escribe una función
+-- (44precioMinPorFabricante) que devuelva el
+-- valor mínimo del precio de los productos de un
+-- determinado fabricante que se recibirá como
+-- parámetro de entrada.
