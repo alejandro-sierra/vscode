edf81cdf542e959cf554b533669b46983db88a73
alejandro-sierra <alejandrosierraballesta@gmail.com>
2021-05-19 14:33:36 +0200

    10p07-08 casi terminado

--- bda12/12p07-08.sql
+++ bda12/12p07-08.sql
@@ -45,19 +45,46 @@ alter table alumnos add edad int;
 
 
 delimiter //
-create or replace function calcularEdad(nacido date)
+create or replace function calcularEdad(nacido date)    
     returns int
 begin
     declare resultado int;
-    declare fechaActual date;
-    select current_date() into fechaActual;
-    set resultado = (datediff(nacido, fechaActual)); 
-    set resultado = truncate(resultado / 365); 
+    set resultado = (datediff(now(),nacido)); 
+    set resultado = truncate(resultado / 365,0); 
     return resultado;
 end;
 //
 delimiter ;
 select calcularEdad('1999-09-23');
+
+/* DATOS DE PRUEBAS
+insert into alumnos values (null(id),nombre,apellido1,apellido2,fechaNaciiento,null(edad));
+
+insert into alumnos values (1,'Elena','Aleksandrova','Gesheva','1998-02-21',null);
+insert into alumnos values (2,'Alejandro','Ballesta','Sierra','1999-09-23',null);
+insert into alumnos values (3,'Pedro','Garcia','Rivera','1981-01-01',null);
+*/
+
+delimiter // --revisar
+create or replace procedure actualizarColumnaEdad()
+begin
+    declare salir int default false;
+    declare fechaCursor int;
+    declare cur cursor for select fechaNacimiento from cursores.alumnos;
+    declare continue handler for not found set salir = true;
+
+    open cur;
+
+    while salir = false do
+        fetch cur into fechaCursor;
+        insert into alumnos set edad="select calcularEdad('fechaCursor')";
+    end while;
+
+    close cur; 
+end;
+//
+delimiter ;
+CALL actualizarColumnaEdad();
 
 /*
 8.- Cursores - email
@@ -84,5 +111,48 @@ tabla.
 (crearListaEmailsAlumnos) que devuelva la lista
 de emails de la tabla alumnos separados por un
 punto y coma.
---Ejemplo:ana@iesseveroochoa.net;bruno@iesseveroochoa.net;carl
+--Ejemplo:ana@iesseveroochoa.net;bruno@iesseveroochoa.net;carlos@iesseveroochoa.net
 */
+delimiter //
+create or replace function crearEmail(nombreF varchar(50),apellido1F varchar(50),apellido2F varchar(50),dominioF varchar(50))  
+    returns varchar(50)
+begin
+    declare nombre varchar(50);
+    declare apellido1 varchar(50);
+    declare apellido2 varchar(50);
+    declare correo varchar(50);
+
+    set nombre = (select substring(nombreF,0,1));
+    set apellido1 = (select substring(apellido1F,0,3));
+    set apellido2 = (select substring(apellido2F,0,3));
+    set correo = lower((concat ('nombre','apellido1','apellido2','@','dominioF','.com')));
+    return correo;
+end;
+//
+delimiter ;
+select crearEmail('Alejandro','Ballesta','Sierra','gmail');
+
+
+alter table alumnos add email varchar(50);
+
+
+delimiter // --revisar
+create or replace procedure actualizarColumnaEmail()
+begin
+    declare salir int default false;
+    declare fechaCursor int;
+    declare cur cursor for select fechaNacimiento from cursores.alumnos;
+    declare continue handler for not found set salir = true;
+
+    open cur;
+
+    while salir = false do
+        fetch cur into fechaCursor;
+        insert into alumnos set edad="select calcularEdad('fechaCursor')";
+    end while;
+
+    close cur; 
+end;
+//
+delimiter ;
+CALL actualizarColumnaEmail();
